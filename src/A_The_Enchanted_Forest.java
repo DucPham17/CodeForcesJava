96eed3c2ecc951376477a6a1c6703ff44d109e10
DucPham17 <phamminhduckl@gmail.com>
2022-09-29 00:29:04 -0500

    A. The Enchanted Forest

--- src/A_The_Enchanted_Forest.java
+++ src/A_The_Enchanted_Forest.java
@@ -31,6 +31,79 @@ public class A_The_Enchanted_Forest {
     }
 
     public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+        Integer t = fs.nextInt();
+        for(int i = 0; i < t; i++){
+            Integer len = fs.nextInt();
+            Integer k = fs.nextInt();
+            int[] arr = new int[len];
+            for(int j = 0; j < len; j++){
+                Integer a = fs.nextInt();
+                arr[j] = a;
+            }
+            if(arr.length != 1){
+                solve(arr,k);
+            }
+            else{
+                long res = (long)(k-1) + (long)arr[0];
+                System.out.println(res);
+            }
+        }
+    }
 
+    public static void solve(int[] arr, int k){
+        int n = arr.length;
+        if(k > n-1){
+            int left = k % (n-1);
+            int count = 0;
+            int total = 0;
+            int[] got = new int[n];
+            long res = 0l;
+            for(int c : arr){
+                res += (long) c;
+            }
+            int cur = 1;
+            boolean minus = true;
+            if(left == 0){
+                left= n-1;
+            }
+            while(count < n && total < k){
+                left -= minus? 1 : -1;
+                long add = (long)cur - (long)got[left];
+                if(got[left] == 0){
+                    add -= 1l;
+                    count += 1;
+                }
+                //  System.out.println(add+" "+left);
+                got[left] = cur;
+                res += add;
+                cur += 1;
+                total += 1;
+                if(left == 0){
+                    minus = false;
+                }
+                if(left == n){
+                    minus = true;
+                }
+                //   System.out.println(res+" "+count+" "+left);
+            }
+            int v = (k - total)/(n-1);
+            res += v > 0? (long)(n-1) * (long)n * (long)v:0l;
+            System.out.println(res);
+        }
+        else{
+            long cur = 0l;
+            for(int i = 0; i < k; i++){
+                cur += (long) arr[i];
+            }
+            long max = cur;
+            for(int i = k; i < n; i++){
+                cur += (long) arr[i];
+                cur -= (long) arr[i-k];
+                max = Math.max(max,cur);
+            }
+            long res = max + (long)k*(long)(k-1)/2;
+            System.out.println(res);
+        }
     }
 }
